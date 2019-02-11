b9c103df932b20c21bcf48131ef1e0b8c702eaf0
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-11 17:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return `Happy holidays, ${name}!`;
 }
 
 function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(days, mother) {
-  `It's ${days} days until ${mother}!`
+  `It's ${days} days until ${mother}!`;
 }
